5c152aedd556343b438fb88e32dc9c4a96920e5a
Alireza <nili_505077@yahoo.com>
2019-03-12 21:33:19 +0330

    commented TD4

--- A3/A3Tests/GradedTests.cs
+++ A3/A3Tests/GradedTests.cs
@@ -18,10 +18,10 @@ namespace A3.Tests
         public void SolveTest()
         {
             Processor[] problems = new Processor[] {
-               //new Q1MinCost("TD1"),
-               //new Q2DetectingAnomalies("TD2"),
-               //new Q3ExchangingMoney("TD3"),
-               new Q4FriendSuggestion("TD4")
+               new Q1MinCost("TD1"),
+               new Q2DetectingAnomalies("TD2"),
+               new Q3ExchangingMoney("TD3"),
+               //new Q4FriendSuggestion("TD4")
             };
 
             foreach (var p in problems)
