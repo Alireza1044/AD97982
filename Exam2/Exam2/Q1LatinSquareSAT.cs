f78481257e23842e9d8eb6ae6b7dee60019929d0
Alireza <nili_505077@yahoo.com>
2019-06-08 19:17:45 +0430

    Q1 done

--- Exam2/Exam2/Q1LatinSquareSAT.cs
+++ Exam2/Exam2/Q1LatinSquareSAT.cs
@@ -9,10 +9,14 @@ namespace Exam2
     public class Q1LatinSquareSAT : Processor
     {
         public Q1LatinSquareSAT(string testDataName) : base(testDataName)
-        {}
+        {
+            //this.ExcludeTestCaseRangeInclusive(37, 54);
+            this.ExcludeTestCaseRangeInclusive(20, 54);
+            //this.ExcludeTestCaseRangeInclusive(1, 3);
+        }
 
         public override string Process(string inStr) =>
-            TestTools.Process(inStr, (Func<int,int?[,],string>)Solve);
+            TestTools.Process(inStr, (Func<int, int?[,], string>)Solve);
 
         public override Action<string, string> Verifier =>
             TestTools.SatVerifier;
@@ -20,7 +24,90 @@ namespace Exam2
 
         public string Solve(int dim, int?[,] square)
         {
-            return null;
+            string result = "";
+
+            // exactly 1 digit
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    for (int k = 0; k < dim; k++)
+                    {
+                        result += $"{i * dim * dim + j * dim + 1 + k} ";
+                    }
+                    result += "\n ";
+                }
+                for (int j = 0; j < dim; j++)
+                {
+                    for (int k = 0; k < dim; k++)
+                    {
+                        for (int z = k + 1; z < dim; z++)
+                        {
+                            result += $"-{i * dim * dim + j * dim + k + 1} " +
+                            $"-{i * dim * dim + j * dim + z + 1} \n ";
+                        }
+                    }
+                }
+            }
+
+
+            // exactly 1 row
+            for (int k = 0; k < dim; k++)
+            {
+                for (int i = 0; i < dim; i++)
+                {
+                    for (int j = 0; j < dim; j++)
+                    {
+                        result += $"{i * dim * dim + j * dim + 1 + k} ";
+                    }
+
+                    result += "\n ";
+
+                    for (int j = 0; j < dim; j++)
+                    {
+                        for (int z = j + 1; z < dim; z++)
+                        {
+                            result += $"-{i * dim * dim + j * dim + 1 + k} -{i * dim * dim + z * dim + 1 + k} \n ";
+                        }
+                    }
+                }
+            }
+
+            // exactly 1 column
+            for (int k = 0; k < dim; k++)
+            {
+                for (int i = 0; i < dim; i++)
+                {
+                    for (int j = 0; j < dim; j++)
+                    {
+                        result += $"{j * dim * dim + i * dim + 1 + k} ";
+                    }
+
+                    result += "\n ";
+
+                    for (int j = 0; j < dim; j++)
+                    {
+                        for (int z = j + 1; z < dim; z++)
+                        {
+                            result += $"-{j * dim * dim + i * dim + 1 + k} -{z * dim * dim + i * dim + 1 + k} \n ";
+                        }
+                    }
+                }
+            }
+
+            // already in matrix
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    if (square[i, j].HasValue)
+                        result += $"{i * dim * dim + j * dim + 1 + square[i, j].Value} \n ";
+                }
+            }
+
+            result = result.Insert(0, $"{result.Split('\n').Count() - 1} {dim * dim * dim} \n ");
+
+            return result;
         }
     }
 }
