6f1141c97ab9afcb6893fa9d74330a12f3342f58
Alireza <nili_505077@yahoo.com>
2019-06-08 15:55:05 +0430

    initial commit

---
+++ Exam2/Exam2/Q1LatinSquareSAT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace Exam2
+{
+    public class Q1LatinSquareSAT : Processor
+    {
+        public Q1LatinSquareSAT(string testDataName) : base(testDataName)
+        {}
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<int,int?[,],string>)Solve);
+
+        public override Action<string, string> Verifier =>
+            TestTools.SatVerifier;
+
+
+        public string Solve(int dim, int?[,] square)
+        {
+            return null;
+        }
+    }
+}
---
+++ Exam2/Exam2/Q2LatinSquareBT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace Exam2
+{
+    public class Q2LatinSquareBT : Processor
+    {
+        public Q2LatinSquareBT(string testDataName) : base(testDataName)
+        {
+            this.ExcludeTestCaseRangeInclusive(28, 120);
+        }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<int,int?[,],string>)Solve);
+
+        public string Solve(int dim, int?[,] square)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
